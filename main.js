99c03151c326ed96f74311ff547504cd10d8ed22
takes2 <takes2.soft@gmail.com>
2015-05-10 14:48:22 +0900

    ランダム追加

--- main.js
+++ main.js
@@ -7,126 +7,90 @@ Core : ゲーム本体
 
 
 */
-
 window.onload = function(){
 
-	var GAMEN_WIDTH = 720;
-	var GAMEN_HEIGHT = 960;
-	var j_flag = false; //ジャンプフラグ
-	var j_frame = 0;	//ジャンプ処理フレーム数
-	var j_frameMax = 6;	//ジャンプ処理フレーム最大
-	var j_pix = 0;		//ジャンプ実移動量
-	
-	var jumpForce = 1;//ジャンプ力
-	var downForce = 10;	//重力
+	GAMEN_WIDTH = 720;
+	GAMEN_HEIGHT = 960;
+	PANDA_GRAV = 5;		//パンダの重力
+	PANDA_JUMP = 30;	//パンダのジャンプ力
+	TAKE_FLG = true;	//竹を出すフラグ
+	TAKE_FREQ = 60;		//竹の出現頻度
+	TAKE_MOVE = 10;		//竹の早さ
+	TAKE_PATT = 32;		//竹のパターン数
+	TAKE_HOLE = 250;	//竹の隙間
+	SCORE_FREQ = 1;		//スコア加点量
 
-	var core = new Core(GAMEN_WIDTH,GAMEN_HEIGHT);
-	core.preload('panda1.png');
-	core.preload('take1.png');
-	core.preload('take2.png');
-	core.fps = 15;
-	core.onload = function(){
-				
-	var scene = core.rootScene;
-	scene.backgroundColor = '#7CC28E';
-				
-		var panda = new Sprite(68,68);
-		panda.image = core.assets['panda1.png'];
+	game = new Game(GAMEN_WIDTH,GAMEN_HEIGHT);
+	game.preload(['panda1.png','take1.png','take2.png','yuka.png']);
+	game.fps = 24;
+	game.score = -1;
+	
+	game.onload = function(){
+		//背景
+		var scene = game.rootScene;
+		scene.backgroundColor = '#7CC28E';
+		
+		//パンダ表示		
+		panda = new Sprite(68,68);
+		panda.image = game.assets['panda1.png'];
 		panda.x = 0;
 		panda.y = 100;
 		panda.frame = 1;
+		panda.vy = 0;
+		panda.jumping = false;
 		
 		panda.addEventListener('enterframe',function(){	
 			//加速度計算
-			if (j_flag == true) {
-				if (j_frame == j_frameMax){
-					j_flag = false;
-					j_pix = 0;
-				}else{
-					j_pix -= jumpForce * (j_frameMax - j_frame) * (j_frameMax - j_frame);
-				}
-				j_frame += 1;
-				this.y += j_pix;
-			};
+			this.vy += PANDA_GRAV;
 			
 			//パンダ位置
 			this.x = 100;
-			this.y += downForce; 	
+			this.y += this.vy; 	
 
 			//画面外制御
 			if(this.x > GAMEN_WIDTH) this.x = 0;
-			if(this.y > GAMEN_HEIGHT) this.y = 0;
+			if(this.y > GAMEN_HEIGHT) this.y = GAMEN_HEIGHT - 75;
 		});
 		
-		//竹基本クラス
-		var Take = Class.create(Sprite,{
-			initialize: function(x,y,f) {
-				Sprite.call(this, 250, 125);		//継承元クラスの初期化
-				this.x = x;
-				this.y = y;
-				this.frame = f;
-				this.image = core.assets['take1.png'];
-				core.rootScene.addChild(this);
-			},
-			onenterframe:function(){				//enterframe時のイベントリスナー
-				this.x -= 10;
-				if (this.x < -250) this.x = GAMEN_WIDTH;
-				if (this.within(panda,100)){
-					lblStatus.text = 'HIT';	
-				}
+		game.rootScene.addEventListener('enterframe',function(){		
+            // フレームごとに竹を増やす関数を実行
+			if(game.frame % TAKE_FREQ == 0){
+				if (TAKE_FLG) addTake(rand(TAKE_PATT));
+            }
+            //点数追加
+            if(game.frame % TAKE_FREQ == 0){
+				game.score += SCORE_FREQ;
 			}
 		});
-		
-				//竹基本クラス
-		var Take2 = Class.create(Sprite,{
-			initialize: function(x,y,f) {
-				Sprite.call(this, 250, 125);		//継承元クラスの初期化
-				this.x = x;
-				this.y = y;
-				this.frame = f;
-				this.image = core.assets['take2.png'];
-				core.rootScene.addChild(this);
-			},
-			onenterframe:function(){				//enterframe時のイベントリスナー
-				this.x -= 10;
-				if (this.x < -250) this.x = GAMEN_WIDTH;
-				if (this.within(panda,100)){
-					lblStatus.text = 'HIT';	
-				}
-			}
-		});
-		
-		//竹表示
-		var takes = [];
-		var iMax = 0
-		
-		for (var i = 0; i <= iMax; i++){
-			takes[i] = new Take(500, GAMEN_HEIGHT - (i * 125),3);
-			
-			if (i == iMax){
-				takes[iMax + 1] = new Take(500, GAMEN_HEIGHT - (i * 125) -125,2);
-				takes[iMax + 2] = new Take(500, GAMEN_HEIGHT - (i * 125) -250,1);
-				takes[iMax + 3] = new Take(500, GAMEN_HEIGHT - (i * 125) -375,0);
-			}
-		}
-
-		iMax = 0
-		var takes2 = [];	
-		for (var i = 0; i <= iMax; i++){
-			takes2[i] = new Take2(650,(i * 125),0);
-			
-			if (i == iMax){
-				takes2[iMax + 1] = new Take2(650, (i * 125),1);
-				takes2[iMax + 2] = new Take2(650, (i * 125) +125,2);
-				takes2[iMax + 3] = new Take2(650, (i * 125) +250,3);
-			}
-		}
 		
 		//パンダジャンプ
-		core.rootScene.addEventListener('touchstart',function(){		
-			j_flag = true;
-			j_frame = 0;
+		game.rootScene.addEventListener('touchstart',function(){		
+			panda.vy = -PANDA_JUMP;
+			panda.jumping = true;
 		});
+		
+		//原基本クラス
+		var Hara = Class.create(Sprite,{
+			initialize: function(x,y,f) {
+				Sprite.call(this, 2000, 100);		//継承元クラスの初期化
+				this.x = x;
+				this.y = y;
+				this.frame = f;
+				this.image = game.assets['yuka.png'];
+				game.rootScene.addChild(this);
+			},
+			onenterframe:function(){				//enterframe時のイベントリスナー
+				this.x -= 10;
+				if (this.x < -1000) this.x = GAMEN_WIDTH;
+				if (this.within(panda,100)){
+					lblStatus.text = 'HIT';	
+				}
+			}
+		});
+		
+		var hara
+		hara = new Hara(0,950,0);
+		hara = new Hara(1000,950,0);
 		
 		//ラベルの操作。
 		var lblScore = new Label();
@@ -136,10 +100,10 @@ window.onload = function(){
 		lblScore.font = '24px "Arial"';
 		lblScore.text = '0'
 		lblScore.addEventListener('enterframe',function(){
-			lblScore.text = (core.frame / core.fps).toFixed(2);
+			lblScore.text = game.score;
 		});
 		
-		var lblStatus = new Label();
+		lblStatus = new Label();
 		lblStatus.x = GAMEN_WIDTH - 100;
 		lblStatus.y = 30;
 		lblStatus.color = 'red';
@@ -147,18 +111,58 @@ window.onload = function(){
 		lblStatus.text = '';
 
 		//rootSceneに追加。
-		core.rootScene.addChild(lblScore);
-		core.rootScene.addChild(lblStatus);
-		core.rootScene.addChild(panda);
-		core.rootScene.addChild(take);
+		game.rootScene.addChild(lblScore);
+		game.rootScene.addChild(lblStatus);
+		game.rootScene.addChild(panda);
 
 	}
-	//core.start();
-	core.debug();
+	game.start();
+	//game.debug();
 
 };
 
-//function作成
-function rand(n) {
-	return Math.floor(Math.random() * (n + 1));
+//引数num番目を開け、竹を生成する関数
+function addTake(num){
+	num += 1;
+
+	var takeOver = new Sprite(220, 1000);
+	takeOver.x = 1000;
+	takeOver.y = 200 + (GAMEN_HEIGHT - (TAKE_HOLE)) * num / TAKE_PATT;
+	takeOver.image = game.assets['take1.png'];
+	
+	takeOver.addEventListener('enterframe', function(e){
+		this.x -= TAKE_MOVE;
+		if(this.intersect(panda)){
+			gameEnd();
+		}
+	});
+	
+	var takeUnder = new Sprite(220, 1000);
+	takeUnder.x = 1000;
+	takeUnder.y = -1000 + (GAMEN_HEIGHT - (TAKE_HOLE)) * num / TAKE_PATT ;
+	takeUnder.image = game.assets['take2.png'];
+	
+	takeUnder.addEventListener('enterframe', function(e){
+		this.x -= TAKE_MOVE;
+		if (this.intersect(panda)){
+			gameEnd();
+		}
+	});
+	
+	game.rootScene.addChild(takeOver);
+	game.rootScene.addChild(takeUnder);
+
+}
+
+function gameEnd(){
+	TAKE_MOVE = 0;
+	SCORE_FREQ = 0;
+	//PANDA_GRAV = 0;
+	PANDA_JUMP = 0;
+	TAKE_FLG = false;
+}
+
+// 引数 num を受け取って、0 から (num - 1) までの乱数を返す関数
+function rand(num){
+    return Math.floor(Math.random() * num);
 }
